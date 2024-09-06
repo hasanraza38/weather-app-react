62859e47370914538258c31d2134412bb51d557d
hasanraza38 <92hasanraza689@gmail.com>
2024-09-07 00:24:18 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Searchbar from './components/searchBar/searchBar'
+
+ const App = () => {
+  return (
+        <>
+        <h1 className='text-black text-center mt-5 text-4xl font-bold'>Weather App</h1>
+        <div className=''>
+           <Searchbar/>
+
+        </div>
+
+
+        </>
+  )
+}
+export default App
